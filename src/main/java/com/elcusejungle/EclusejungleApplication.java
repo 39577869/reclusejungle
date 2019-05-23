81c3ad42ded5f7bccf99bdeb72a122c33f951f58
fengbaobao <2580619995@qq.com>
2019-05-23 21:24:53 +0800

    1

---
+++ src/main/java/com/elcusejungle/EclusejungleApplication.java
@@ -0,0 +1,12 @@
+package com.elcusejungle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclusejungleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EclusejungleApplication.class,args);
+    }
+}

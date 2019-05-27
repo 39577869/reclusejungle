569dc34c1e467c9428929fccc7ad7b10d37b0ffa
tom_glb <goodMorning_pro@atguigu.com>
2019-05-27 09:40:34 +0800

    小修改

--- src/main/java/com/elcusejungle/config/WebConfig.java
+++ src/main/java/com/elcusejungle/config/WebConfig.java
@@ -1,20 +1,16 @@
 package com.elcusejungle.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * 拦截器配置类
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-//        registry.addInterceptor()
-//                .addPathPatterns("/**");
-        super.addInterceptors(registry);
-    }
 
+    }
 }
--- src/test/java/com/eclusejungle/EclusejungleApplicationTests.java
+++ src/test/java/com/eclusejungle/EclusejungleApplicationTests.java
@@ -11,6 +11,7 @@ public class EclusejungleApplicationTests {
 
     @Test
     public void contextLoads() {
+
     }
 
 }
